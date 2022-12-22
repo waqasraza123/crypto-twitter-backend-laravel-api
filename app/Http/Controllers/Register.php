e182290bc480c6cb11afee1f85ae4db034451443
Waqas Raza <waqasraza123@gmail.com>
2022-12-22 18:45:40 +0500

    registration complete

--- app/Http/Controllers/Register.php
+++ app/Http/Controllers/Register.php
@@ -2,11 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class Register extends Controller
 {
-    public function register(){
-        return "hello";
+    public function register(Request $request){
+
+        //validate the incoming data
+        $validated = $request->validate([
+            "name" => "required|min:3|max:40",
+            "username" => "required|min:3|max:20|unique:users,username",
+            "email" => "required|email|unique:users,email",
+            "password" => "required|min:6|max:50"
+        ]);
+
+        //data is validated
+        //store in the db
+        $user = new User();
+        $user->name = $request->input("name");
+        $user->username = $request->input("username");
+        $user->email = $request->input("email");
+        $user->password = Hash::make($request->input("password"));
+        $user->save();
+
+        //create token for the user
+        $token = $user->createToken($request->email)->plainTextToken;
+        $user->accessToken = $token;
+
+        //return response
+        return response()->json([
+            "message" => "User Registered",
+            "user" => $user
+        ]);
     }
 }
